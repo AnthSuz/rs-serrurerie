f28b64c983e963b551ada533927d936ddcc4917f
AnthSuz <56254134+AnthSuz@users.noreply.github.com>
2024-07-22 16:35:10 +0200

    Adding the first sections components to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,47 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+"use client";
+import Acceuil from "@/components/acceuil";
+import Contact from "@/components/contact";
 import Header from "@/components/header";
+import Services from "@/components/services";
+import Image from "next/image";
+import WhatsappLogo from "../../public/img/whatsapp.256x256.png";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [displayContactMe, setDisplayContactMe] = useState<Boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => setDisplayContactMe(false), 5000);
+  }, []);
   return (
     <>
       <Header />
+      <Acceuil />
+      <Services />
+      <Contact />
+      <div className="fixed right-2 bottom-2 ">
+        <p
+          className={`${
+            displayContactMe
+              ? "border border-black rounded bg-white mb-1 text-[10px] uppercase p-0.5"
+              : "hidden"
+          } duration-200`}
+        >
+          Contact moi 🙂
+        </p>
+        <a
+          href="https://wa.me/666150874"
+          className="hover:scale-110 duration-200"
+        >
+          <Image
+            src={WhatsappLogo}
+            width={60}
+            alt="contact via whatsapp"
+            className="cursor-pointer hover:scale-110 duration-200 mx-auto"
+          />
+        </a>
+      </div>
+
       {/* Ajouter section Acceuil */}
       {/* Ajouter section Services */}
       {/* Ajouter section Certainement tarifs??? */}
