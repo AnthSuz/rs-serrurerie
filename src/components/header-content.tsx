492e0b5a70dfaa7b108fda34c19d43c2dd2c1762
AnthSuz <56254134+AnthSuz@users.noreply.github.com>
2024-08-13 19:36:39 +0200

    Update responsive header

--- src/components/header-content.tsx
+++ src/components/header-content.tsx
@@ -1,36 +1,49 @@
 import { Separator } from "@/components/ui/separator";
-import { montserrat } from "@/app/layout";
 
 interface headerContent {
-    orientation: 'vertical' | 'horizontal',
-    closeSheet: () => void,
+  orientation: "vertical" | "horizontal";
+  closeSheet: () => void;
 }
 
-export default function HeaderContent({ orientation, closeSheet }: headerContent) {
+export default function HeaderContent({
+  orientation,
+  closeSheet,
+}: headerContent) {
   return (
     <>
-      <p
-        className={`text-lg cursor-pointer hover:underline underline-offset-4 hover:decoration-[#B91F25] text-[#020202] ${montserrat.className} text-base lg:text-lg`}
+      <a
+        href="#accueil"
+        className="text-lg cursor-pointer hover:underline underline-offset-4 hover:decoration-[#E1E1E1] text-white text-base lg:text-lg"
+        onClick={closeSheet}
       >
         ACCEUIL
-      </p>
+      </a>
       <Separator orientation={orientation} decorative className="bg-white" />
-      <a href="#services"
-        className={`text-lg cursor-pointer hover:underline underline-offset-4 hover:decoration-[#B91F25] text-[#020202] ${montserrat.className} text-base lg:text-lg`}
+      <a
+        href="#services"
+        className="text-lg cursor-pointer hover:underline underline-offset-4 hover:decoration-[#E1E1E1] text-white text-base lg:text-lg"
         onClick={closeSheet}
       >
         NOS SERVICES
       </a>
       <Separator orientation={orientation} decorative className="bg-white" />
-      <p
-        className={`text-lg cursor-pointer hover:underline underline-offset-4 hover:decoration-[#B91F25] text-[#020202] ${montserrat.className} text-base lg:text-lg`}
+      <a
+        href="#tarifs"
+        className="text-lg cursor-pointer hover:underline underline-offset-4 hover:decoration-[#E1E1E1] text-white text-base lg:text-lg"
+        onClick={closeSheet}
+      >
+        TARIFS
+      </a>
+      <Separator orientation={orientation} decorative className="bg-white" />
+      <a
+        href="#contact"
+        className="text-lg cursor-pointer hover:underline underline-offset-4 hover:decoration-[#E1E1E1] text-white text-base lg:text-lg"
+        onClick={closeSheet}
       >
         CONTACT
-      </p>
+      </a>
       <Separator orientation={orientation} decorative className="bg-white" />
-      <p
-        className={`text-lg cursor-default text-slate-400 ${montserrat.className} text-base lg:text-lg`}
-      >
+      <p className="text-lg cursor-default text-slate-400 text-base lg:text-lg">
         DEVIS EN LIGNE (bientôt)
       </p>
     </>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,9 @@
-'use client'
-import RsLogo from "../../public/img/test-img.png";
+"use client";
+import RsLogo from "../../public/img/logo-rs.png";
+import RsLogoMobile from "../../public/img/logo-rs-mobile.png";
+
 import Image from "next/image";
-import { montserrat } from "@/app/layout";
-import { Menu, Phone } from "lucide-react";
+import { Menu } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -14,37 +15,58 @@ import { useState } from "react";
 
 export default function Header() {
   const [openSheet, setOpenSheet] = useState<boolean>(false);
+  const [topPosition, setTopPosition] = useState<number>(0);
 
   const closeSheet = () => {
-    setOpenSheet(false)
-  }
+    setOpenSheet(false);
+  };
+
+  window.addEventListener("scroll", () => {
+    let scroll = window.scrollY;
+    setTopPosition(scroll);
+  });
+
   return (
-    <div className="flex gap-2 justify-between fixed  px-8 p-2 w-full bg-[#e1e1e1] text-white">
-      <div className="flex justify-center items-center">
-        <Image src={RsLogo} alt="rs serrurie" className="w-12 md:w-24" />
-      </div>
-      <div className="gap-4 justify-center items-center hidden md:flex">
-        <HeaderContent orientation="vertical" closeSheet={closeSheet} />
-      </div>
-      <div className="flex gap-2 justify-center items-center hidden md:flex">
-        <Phone color="#B91F25" />
-        <a
-          className={`text-lg cursor-pointer font-medium hover:underline underline-offset-4 hover:decoration-[#B91F25] text-[#020202] ${montserrat.className} text-base lg:text-lg`}
-          href="tel:+33666150874"
+    <div>
+      <div className="bg-[#BF1E1D] flex fixed w-full justify-between p-2 md:p-0 md:block scroll-smooth">
+        <div
+          className={`flex justify-center hidden ${
+            topPosition > 50 ? "hidden" : "md:flex"
+          }`}
         >
-          06.66.15.08.74
-        </a>
+          <Image
+            src={RsLogo}
+            alt="rs serrurie"
+            style={{ width: "200px", transition: "ease-out" }}
+            className="pt-3"
+          />
+        </div>
+        <div className="flex justify-center block md:hidden">
+          <Image
+            src={RsLogoMobile}
+            alt="rs serrurie"
+            style={{ width: "250px" }}
+            className="p-4"
+          />
+        </div>
+        <div className="gap-6 justify-center items-center hidden md:flex py-3">
+          <HeaderContent orientation="vertical" closeSheet={closeSheet} />
+        </div>
+        <Sheet open={openSheet} onOpenChange={() => setOpenSheet(!openSheet)}>
+          <SheetTrigger
+            className="md:hidden"
+            onClick={() => setOpenSheet(true)}
+          >
+            <Menu color="white" />
+          </SheetTrigger>
+          <SheetContent side={"right"} className="bg-[#BF1E1D]">
+            <SheetDescription className="gap-4 flex flex-col">
+              <HeaderContent orientation="horizontal" closeSheet={closeSheet} />
+            </SheetDescription>
+          </SheetContent>
+        </Sheet>
       </div>
-      <Sheet open={openSheet}>
-        <SheetTrigger className="md:hidden" onClick={() => setOpenSheet(true)}  >
-          <Menu color="#020202" />
-        </SheetTrigger>
-        <SheetContent side={"right"} className="bg-[#e1e1e1]">
-          <SheetDescription className="gap-4 flex flex-col">
-            <HeaderContent orientation="horizontal" closeSheet={closeSheet} />
-          </SheetDescription>
-        </SheetContent>
-      </Sheet>
+      <div className={`bg-[#BF1E1D] h-[63.73px] w-full md:h-[173.05px]`}>.</div>
     </div>
   );
 }
