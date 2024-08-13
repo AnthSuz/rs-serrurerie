feb3347a2ca087e394dda8a1034da89ed7349b0e
AnthSuz <56254134+AnthSuz@users.noreply.github.com>
2024-08-13 18:59:12 +0200

    Add title and description to website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,34 +1,39 @@
-import "./globals.css"
-import { Inter as FontSans } from "next/font/google"
-import { Montserrat } from "next/font/google"
+import "./globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const montserrat = Montserrat({
   subsets: ["latin"],
-  display: 'swap',
-  variable: '--font-montserrat'
-})
+  display: "swap",
+  variable: "--font-montserrat",
+});
 
-export default function RootLayout({ children }: Readonly<{
+export default function RootLayout({
+  children,
+}: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <title>RS Serrurerie</title>
+      <meta name="description" content="RS Serrurerie, votre serrurier professionnel pour dépannage, installation et conseils en serrurerie dans toute l'ile de France. Disponibilité 24/7." />
       <head />
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable
+          fontSans.variable,
+          montserrat.className
         )}
       >
         {children}
       </body>
     </html>
-  )
+  );
 }
